28013ec6e55b3f902143f35f3f3546649c15bb47
Matt Clark <matt@mattclark.net>
2021-12-01 17:12:52 +0800

    day 1 solved

--- 1.py
+++ 1.py
@@ -1,1 +1,18 @@
+with open("1-input.txt") as f:
+    input = [int(l) for l in f.readlines()]
+print(input)
 
+# puzzle 1
+m = 0
+for (a, b) in zip(input, input[1:]):
+    if b > a:
+        m += 1
+print(m)
+
+# puzzle 2
+m = 0
+windows = [a * b * c for a, b, c in zip(input, input[1:], input[2:])]
+for (a, b) in zip(windows, windows[1:]):
+    if b > a:
+        m += 1
+print(m)
